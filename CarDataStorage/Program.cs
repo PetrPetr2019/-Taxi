2af219a5eb0b4bce59394bf68e96b6b4ee3777ba
PetrPetr <ferel2015@yandex.ru>
2019-06-06 18:50:14 +0300

    Добавление данных в БД  Truck

--- CarDataStorage/Program.cs
+++ CarDataStorage/Program.cs
@@ -10,6 +10,11 @@ namespace CarDataStorage
     {
         static void Main(string[] args)
         {
+            List<Truck> trucks = new List<Truck>
+            {
+                new Truck{ModelAvto = "Камаз", Carrying = 20, DataofIssue = 1999, DataofMajorOverhaur = 2015, NumberGos = 374532, EncryptPark = 23, ID = 1},
+                new Truck{ModelAvto = "Белаз", Carrying = 200, DataofIssue = 2000, DataofMajorOverhaur = 2018, NumberGos = 34853, EncryptPark =12, ID = 2}
+            };
         }
     }
 }
