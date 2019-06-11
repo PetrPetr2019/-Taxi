526d89f7176c6f731015e177e1137a602dfe70dd
PetrPetr <ferel2015@yandex.ru>
2019-06-11 16:56:25 +0300

    Добавлен запрос на соединения 2 сущностей. оператор join и  оператор  equlas

--- CarDataStorage/Program.cs
+++ CarDataStorage/Program.cs
@@ -12,13 +12,13 @@ namespace CarDataStorage
         {
             List<Truck> trucks = new List<Truck>
             {
-                new Truck{ModelAvto = "Камаз", Carrying = 20, DataofIssue = 1999, DataofMajorOverhaur = 2015, NumberGos = 374532, EncryptPark = 23, ID = 1},
-                new Truck{ModelAvto = "Белаз", Carrying = 200, DataofIssue = 2000, DataofMajorOverhaur = 2018, NumberGos = 34853, EncryptPark =12, ID = 2}
+                new Truck{ModelAvto = "Камаз", Carrying = 20, DataofIssue = 1999, DataofMajorOverhaur = 2015, NumberGos = 374532, EncryptPark = 23},
+                new Truck{ModelAvto = "Белаз", Carrying = 200, DataofIssue = 2000, DataofMajorOverhaur = 2018, NumberGos = 34853, EncryptPark =12}
             };
             List<Taxi> taxis = new List<Taxi>
             {
-              new Taxi{MarkaAvto = "Audi - 6", NumberPassengerSeat = 5, Data = 2000, GosNUmber = "С 065MK-78rus", EncryptPark = 55, ID =1},
-              new Taxi{MarkaAvto ="BMW", NumberPassengerSeat = 4, Data = 2017, GosNUmber = "АН 7331-47КRus", EncryptPark = 45, ID = 2},
+              new Taxi{MarkaAvto = "Audi - 6", NumberPassengerSeat = 5, Data = 2000, GosNUmber = "С 065MK-78rus", EncryptPark = 55},
+              new Taxi{MarkaAvto ="BMW", NumberPassengerSeat = 4, Data = 2017, GosNUmber = "АН 7331-47КRus", EncryptPark = 45},
               new Taxi{MarkaAvto = "Lada Granta", NumberPassengerSeat = 6, Data = 1975, GosNUmber = "Н 700УУ-37rus"}
             };
             List<Autopark> autoparks = new List<Autopark>
@@ -26,16 +26,30 @@ namespace CarDataStorage
                 new Autopark{NamePark = "7-автобусный парк", AdressPlacement = "Город Москва", SqerAreaAvto = 2000, Encrypt =45, ID = 1},
                 new Autopark{NamePark = "33-авто парк", AdressPlacement = "Новые черемушки", SqerAreaAvto = 100, Encrypt = 33, ID = 2}
             };
-            var Query = from TaxiNumber in taxis // Подсчет количества машин в парке
-                        group TaxiNumber by TaxiNumber.MarkaAvto into t
+            var Query = from Trusks  in  trucks
+                        join T_autopark in  taxis on Trusks.ID equals T_autopark.ID
                         select new
                         {
-                            t.Key,
+                            Trusks.ModelAvto,
+                            T_autopark.MarkaAvto,
+                            T_autopark.NumberPassengerSeat,
                         };
+            foreach (var rez in Query)
+            {
+                Console.WriteLine("\n{0}-{1}-{2}", rez.ModelAvto, rez.NumberPassengerSeat, rez.MarkaAvto);
+            }
+            Console.ReadLine();
+          
+            //var Query = from TaxiNumber in taxis // Подсчет количества машин в парке
+            //            group TaxiNumber by TaxiNumber.MarkaAvto into t
+            //            select new
+            //            {
+            //                t.Key,
+            //            };
 
-            int count = taxis.Count();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Количество машин в парке\n" + count);
+            //int count = taxis.Count();
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.Write("Количество машин в парке\n" + count);
 
 
             // Группировка данных  с использованием group
@@ -63,7 +77,7 @@ namespace CarDataStorage
             //{
             //    Console.WriteLine("\n{0}", Ca.Data);
             //    Console.BackgroundColor = ConsoleColor.Red;
-            Console.ReadLine();
+           
         }
     }
 }
